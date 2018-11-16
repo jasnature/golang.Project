9cd8b3b456872b663e2114fe505591001088c699
刘敬 <276227015@qq.com>
2018-11-16 18:02:23 +0800

    Update main.go

--- ConnProxy/main.go
+++ ConnProxy/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"connProxy/base"
 	"flag"
 	"fmt"
 )
@@ -19,11 +20,14 @@ func main() {
 
 	flag.StringVar(&ip, "ip", " ", "please input listen ip. def(all ip)")
 	flag.StringVar(&pt, "pt", "9696", "please input listen port.def(9696)")
-	flag.IntVar(&plog, "plog", 1, "please set print log status. def(0-disable 1-enable)")
+	flag.IntVar(&plog, "plog", 0, "please set print log status. def(0-disable 1-enable)")
 	flag.IntVar(&buf, "buf", 1024*16, "please set print log status. def(1024*16k)")
 	flag.StringVar(&allowIp, "allip", "*", "allow * or access ip address list(use , split(etc. 10.21.30.159,10.21.30.160,10.21.30.151).)")
 	flag.Parse()
-	proxy := &ProxyServer{linkCount: 0, addr: ip, port: pt, printLog: plog == 1, buffSize: buf, allowIpStr: allowIp}
+
+	//linkCount: 0, addr: ip, port: pt, printLog: plog == 1, buffSize: buf, allowIpStr: allowIp
+	configObj := base.ProxyConfig{Addr: ip, Port: pt, PrintLog: plog == 1, BuffSize: buf, AllowIpStr: allowIp}
+	proxy := &ProxyServer{config: configObj}
 
 	proxy.StartProxy()
 
