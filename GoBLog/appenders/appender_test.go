d537ea0db1f7f18d615d21787697f0357946a6ca
liujing <276227015@qq.com>
2019-01-03 17:39:37 +0800

    Update appender_test.go

--- GoBLog/appenders/appender_test.go
+++ GoBLog/appenders/appender_test.go
@@ -4,20 +4,36 @@ import (
 	"GoBLog/base"
 	"GoBLog/formatters"
 	"testing"
+	"time"
 )
 
 func TestConsole(t *testing.T) {
 	console := NewConsoleAppender()
-	console.Write(base.DEBUG, "TestConsole=%d", 666)
+	console.WriteString(base.DEBUG, "TestConsole=%d", 666)
 
 	console.SetFormatter(formatters.DefaultPatternFormatter())
-	console.Write(base.TRACE, "TestConsole=%d", 666)
+	console.WriteString(base.TRACE, "TestConsole=%d", 666)
 }
 
 func TestFileRolling(t *testing.T) {
-	console := NewFileAppender("goblog.log", true, 1024)
-	console.Write(base.DEBUG, "TestFileRolling=%d", 666)
+	filer, err := NewFileAppender("goblog.log", true, 1024)
+	filer.WriteString(base.DEBUG, "TestFileRolling=%d,err=%v", 666, err)
 
-	console.SetFormatter(formatters.DefaultPatternFormatter())
-	console.Write(base.TRACE, "TestFileRolling=%d", 666)
+	filer.SetFormatter(formatters.DefaultPatternFormatter())
+	for i := 0; i < 3; i++ {
+		filer.WriteString(base.TRACE, "TestFileRolling=%d", i)
+	}
+}
+
+func TestMutiple(t *testing.T) {
+	console := NewConsoleAppender()
+	filer, _ := NewFileAppender("goblog.log", true, 1024)
+
+	mutiple := NewMultipleAppender(100, console, filer)
+
+	for i := 0; i < 3; i++ {
+		mutiple.WriteString(base.INFO, "TestMutiple=%d %s %d", i, "kkkkkkkkkkk", 123456)
+	}
+
+	time.Sleep(time.Second * 1)
 }
