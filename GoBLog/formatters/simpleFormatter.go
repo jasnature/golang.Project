30278d7c621d08908c5903da853bb49bff170eaf
liujing <276227015@qq.com>
2019-01-03 17:42:21 +0800

    Update simpleFormatter.go

--- GoBLog/formatters/simpleFormatter.go
+++ GoBLog/formatters/simpleFormatter.go
@@ -15,6 +15,6 @@ func NewSimpleFormatter() *SimpleFormatter {
 }
 
 func (this *SimpleFormatter) Format(level base.LogLevel, message string, args ...interface{}) string {
-	//return fmt.Sprintf("%s %s", base.DefaultUtil().NowTimeStr(), fmt.Sprintf(message, args...))
-	return fmt.Sprintf(message, args...)
+	return fmt.Sprintf("[%s] %s %s \n", base.LogLevelIntMap[level], base.DefaultUtil().NowTimeStr(), fmt.Sprintf(message, args...))
+	//return fmt.Sprintf(message, args...)
 }
