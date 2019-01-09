e2e9144b7111b30e217fa8e966287e8ecdb2df4f
liujing <276227015@qq.com>
2019-01-09 17:56:31 +0800

    Update appender.go

--- GoBLog/appenders/appender.go
+++ GoBLog/appenders/appender.go
@@ -11,3 +11,9 @@ import (
 type Appender interface {
 	WriteString(level base.LogLevel, message string, args ...interface{})
 }
+
+//auto date filename and append model
+//buffer 64KB, file size max 6MB
+func DefaultFileAppender() (Appender, error) {
+	return NewFileAppender("", true)
+}
