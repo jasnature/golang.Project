06e6ce4163c98fa0d5d4cf90255220940d5f850f
liujing <276227015@qq.com>
2019-01-09 18:01:06 +0800

    Update formatters_test.go

--- GoBLog/formatters/formatters_test.go
+++ GoBLog/formatters/formatters_test.go
@@ -7,12 +7,12 @@ import (
 
 func TestSimple(t *testing.T) {
 
-	str := NewSimpleFormatter().Format(base.DEBUG, "My type it is float=%f", 123.345)
+	str := NewSimpleFormatter().Format(base.INFO, "My type it is float=%f", 123.345)
 	t.Log(str)
 }
 
 func TestBasic(t *testing.T) {
-	var pattern string = "%p %d %l %n LogInfo: %m"
+	var pattern string = "%p %d{15:04:05.000} %l %n LogInfo: %m \n"
 
 	str := NewPatternFormatter(pattern).Format(base.DEBUG, "NewBasicFormatter log=%d", 123456)
 	str1 := DefaultPatternFormatter().Format(base.TRACE, "NewBasicFormatter log=%d", 123456)
