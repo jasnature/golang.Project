88ab1782861b75d4ff326698479c743f5e6bd6f8
liujing <276227015@qq.com>
2019-01-09 17:58:09 +0800

    Update base_test.go

--- GoBLog/base/base_test.go
+++ GoBLog/base/base_test.go
@@ -20,8 +20,18 @@ func TestLevels(t *testing.T) {
 	b := TRACE
 	t.Log("LogLevel =", a, "Single Declare:", a == b)
 }
+
 func TestUtil(t *testing.T) {
 	t.Log(DefaultUtil() == &Util{})
 	t.Log(DefaultUtil() == DefaultUtil())
-	t.Log(DefaultUtil().NowTimeStr())
+	t.Log(DefaultUtil().NowTimeStr(0))
+	t.Log(DefaultUtil().NowTimeStr(1))
+	t.Log(DefaultUtil().NowTimeStr(2))
+	t.Log(DefaultUtil().NowTimeStr(3))
+}
+func TestByteSize(t *testing.T) {
+	t.Logf("Byte:%d", Byte)
+	t.Logf("KB:%d", KB)
+	t.Logf("MB:%d", MB)
+	t.Logf("GB:%d", GB)
 }
